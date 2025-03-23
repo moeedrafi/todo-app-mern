8f90fc157252a77691188e3852c86ae7ac600ad3
Moeed Rafi <moeedrafi95@gmail.com>
2025-03-23 13:07:15 +0500

    feat: multer middleware

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,19 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async () => {});
+const registerUser = asyncHandler(async (req, res) => {
+  /*
+    if uploaded avatar
+    if user already exists
+    add to db
+    */
+
+  const { fullName, username, email, password } = req.body;
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+});
 
 export { registerUser };
