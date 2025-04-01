73cb70dd5a3aadd5553f18c7a14e76f5acb85138
Moeed Rafi <moeedrafi95@gmail.com>
2025-04-01 17:07:24 +0500

    chore: Setup React Router and Absolute Imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router";
+
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+
+import { Navbar } from "@/components/Navbar";
+
 const App = () => {
-  return <div className="text-red-500 bg-amber-400">App</div>;
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,3 @@
+export const Navbar = () => {
+  return <div>Navbar works hooray</div>;
+};
