7a6b39f4e1532496165b28e16a4b585a8499ad88
Moeed Rafi <moeedrafi95@gmail.com>
2025-04-01 18:19:01 +0500

    feat: Navbar not in Login Register Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router";
 
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 
 import { Navbar } from "@/components/Navbar";
 
-const App = () => {
+const AppLayout = () => {
+  const location = useLocation();
+  const hideNavbarRoutes = ["/login", "/register"];
+
   return (
-    <Router>
-      <Navbar />
+    <>
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
 
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <AppLayout />
     </Router>
   );
 };
