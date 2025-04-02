8596e772d98216b59082f3699d279be26027a8e4
Moeed Rafi <moeedrafi95@gmail.com>
2025-04-02 19:14:24 +0500

    feat: Register Functionality

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,26 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useActionState, useState } from "react";
 
+import { registerUserAction } from "@/utils";
 import { Button } from "@/components/ui/button";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [avatar, setAvatar] = useState<File | null>(null);
+  const [state, registerUser, isPending] = useActionState(registerUserAction, {
+    user: null,
+    success: false,
+    message: "",
+    error: false,
+  });
+
+  const formAction = (formData: FormData) => {
+    registerUser({ formData });
+    navigate("/");
+  };
+
+  console.log(state.user);
+
   return (
     <div className="bg-black text-white h-screen w-screen flex items-center justify-center">
       <div className="bg-[#020024] p-4 rounded-lg w-2/4">
@@ -10,7 +28,17 @@ const Register = () => {
           Sign Up
         </h1>
 
-        <div className="flex flex-col gap-3 mb-3">
+        <form action={formAction} className="flex flex-col gap-3 mb-3">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              placeholder="John Doe"
+              className="p-2 rounded-lg"
+            />
+          </div>
+
           <div className="flex flex-col gap-1">
             <label htmlFor="username">Username</label>
             <input
@@ -40,18 +68,40 @@ const Register = () => {
               className="p-2 rounded-lg"
             />
           </div>
-        </div>
 
-        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="avatar">Avatar</label>
+            <input
+              type="file"
+              name="avatar"
+              accept="image/*"
+              className="p-2 rounded-lg"
+              onChange={(e) => {
+                if (e.target.files?.[0]) {
+                  setAvatar(e.target.files[0]);
+                }
+              }}
+            />
+          </div>
+
           <Button
+            type="submit"
             variant="secondary"
+            disabled={isPending}
             className="bg-[#3a31d8] text-white hover:bg-[#0600c2] cursor-pointer"
           >
-            Sign Up
+            {isPending ? "Registring" : "Sign Up"}
           </Button>
 
+          {state.error && <p className="text-red-500">Registration Failed</p>}
+          {state.success && (
+            <p className="text-green-500">User Registered Successfully</p>
+          )}
+        </form>
+
+        <div className="flex justify-center">
           <Button variant="link">
-            <Link to="/register" className="text-white text-center text-xs">
+            <Link to="/register" className="text-white text-xs">
               Already have an Account?
             </Link>
           </Button>
