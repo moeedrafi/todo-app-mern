e7995f68c5e80fcf371234a202215f50f859944f
Moeed Rafi <moeedrafi95@gmail.com>
2025-04-26 18:53:40 +0500

    fix: UI Modal

--- frontend/src/components/navbar/DropdownMenu.tsx
+++ frontend/src/components/navbar/DropdownMenu.tsx
@@ -9,7 +9,7 @@ export const DropdownMenu = () => {
 
   return (
     <>
-      <div className="absolute group-hover:block top-20 right-10 z-10 text-black rounded-lg shadow-md">
+      <div className="absolute group-hover:block top-20 right-10 lg:right-40 z-10 text-black rounded-lg shadow-md">
         <ul className="p-2 bg-gray-100 text-sm flex flex-col items-start">
           <button
             onClick={logout}
